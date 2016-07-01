9803fdd0c1000f2d561b73bf13a652e67f47ca59
mhurd <mike@mhurd.com>
2016-07-01 08:23:07 +0100

    Added initial files.has

---
+++ files.hs
@@ -0,0 +1,40 @@
+import System.Directory
+import Control.Monad
+import Data.List
+
+getFiles :: [FilePath] -> IO [FilePath]
+getFiles = filterM (doesFileExist)
+
+getDirectories :: [FilePath] -> IO [FilePath]
+getDirectories = filterM (doesDirectoryExist)
+
+prependParentDir :: FilePath -> FilePath -> FilePath
+prependParentDir p c = p ++ ("/" ++ c)
+
+removeSpecialDirs :: [FilePath] -> [FilePath]
+removeSpecialDirs = filter (\d -> not $ d `elem` [".", ".."])
+
+makeFullPathAndSort :: FilePath -> [FilePath] -> [FilePath]
+makeFullPathAndSort p = sort . map (prependParentDir p)
+
+processDir :: FilePath -> IO ()
+processDir d = do
+    printDir d
+    contents <- getDirectoryContents d
+    directories <- getDirectories contents
+    let cleanDirectories = makeFullPathAndSort d $ removeSpecialDirs directories
+    mapM_ processDir cleanDirectories
+
+printDir :: FilePath -> IO () 
+printDir d = do
+    putStrLn ""
+    putStrLn $ "current directory:   " ++ d
+    contents <- getDirectoryContents d
+    files <- getFiles $ makeFullPathAndSort d contents
+    directories <- getDirectories $ makeFullPathAndSort d $ removeSpecialDirs contents
+    let listing = (map ("             file:   " ++) files) ++ (map ("        directory:   " ++) directories)
+    mapM_ putStrLn $ listing
+
+main = do
+    currentDirectory <- getCurrentDirectory
+    processDir currentDirectory
