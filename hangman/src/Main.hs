9bd31c75d0479197cd4ade69797eee01d3ba6bf1
Hurd <MHurd@bgcpartners.com>
2017-11-17 13:36:40 +0000

    Working
    Some tweaks to ensure library words are all alpha

--- hangman/src/Main.hs
+++ hangman/src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Monad (forever)
-import Data.Char (toLower)
+import Data.Char (toLower, ord)
 import Data.Maybe (isJust)
 import Data.List (intersperse)
 import System.Exit (exitSuccess)
@@ -9,11 +9,88 @@ import System.Random (randomRIO)
 
 type WordList = [String]
 
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+instance Show Puzzle where
+    show (Puzzle _ discovered guessed) =
+        (intersperse ' ' $
+            fmap renderPuzzleChar discovered)
+        ++ " Guessed so far: " ++ guessed
+
 minWordLength :: Int
 minWordLength = 5
 
 maxWordLength :: Int
-maxWordLength = 9
+maxWordLength = 7
+
+main :: IO ()
+main = do
+    word <- randomWord'
+    let puzzle = freshPuzzle word
+    runGame puzzle
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+    gameOver puzzle
+    gameWin puzzle
+    putStrLn $ "Current puzzle is: " ++ show puzzle
+    putStr "Guess a letter: "
+    guess <- getLine
+    case guess of
+        [c] -> handleGuess puzzle c >>= runGame
+        _ -> putStrLn "Your guess must be a single character"
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+    if length guessed > maxWordLength then
+        do
+            putStrLn "You lose!"
+            putStrLn $ "The word was: " ++ wordToGuess
+            exitSuccess
+    else return ()
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) =
+    if all isJust filledInSoFar then
+        do putStrLn "You win!"
+           exitSuccess
+    else return ()
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+    putStrLn $ "Your guess was: " ++ [guess]
+    case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+        (_, True) -> do
+            putStrLn "You already guessed that character, pick something else!"
+            return puzzle
+        (True, _) -> do 
+            putStrLn "This character was in the word, filling accordingly."
+            return (fillInCharacter puzzle guess)
+        (false, _) -> do
+            putStrLn "The character wasn't in the word, please try again."
+            return (fillInCharacter puzzle guess)
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c = Puzzle word newFilledInSoFar (c : s)
+    where zipper guessed wordChar guessChar =
+            if wordChar == guessed 
+            then Just wordChar
+            else guessChar
+          newFilledInSoFar =
+            zipWith (zipper c) word filledInSoFar
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ g) c = elem c g
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle w _ _) c = elem c w
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing = '_'
+renderPuzzleChar (Just c) = c
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle s = Puzzle s (map (const Nothing) s) []
 
 allWords :: IO WordList
 allWords = do
@@ -23,12 +100,23 @@ allWords = do
 gameWords :: IO WordList
 gameWords = do
   aw <- allWords
-  return (filter gameLength aw)
+  let filtered = filter gameFilter aw
+  return (map (fmap toLower) filtered)
   where gameLength w = 
           let l = length (w :: String)
           in l >= minWordLength
              && l < maxWordLength
+        alphaChar c = 
+            (ord (c :: Char)) >= 97 && (ord (c :: Char)) <= 122
+        alpha w = 
+            all alphaChar (w :: String)
+        gameFilter w = 
+            (gameLength w) && (alpha w)
 
-main :: IO ()
-main = do                                                                                        
-  putStrLn "hello world"
+randomWord :: WordList -> IO String
+randomWord wl = do
+    randomIndex <- randomRIO (0, (length wl) - 1)
+    return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
